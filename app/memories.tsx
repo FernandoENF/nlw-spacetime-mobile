995f07efb573a5163dea3315df9bbf4cc7b87df3
FernandoENF <fernandoengh@gmail.com>
2023-05-21 20:04:56 -0300

    Alterações feitas durante a Aula 4

--- app/memories.tsx
+++ app/memories.tsx
@@ -3,7 +3,9 @@ import { View, Text } from 'react-native'
 export default function Memories() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text>Memories</Text>
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Memories
+      </Text>
     </View>
   )
 }
